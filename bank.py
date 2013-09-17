112215ef7a6af1235f9c9224e92769d6cd9d20d2
resteve <resteve@zikzakmedia.com>
2013-09-17 09:42:17 +0200

    pyflakes

--- bank.py
+++ bank.py
@@ -1,8 +1,8 @@
 #This file is part of company_bank module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
-from trytond.model import ModelView, ModelSQL, fields
-from trytond.pool import Pool, PoolMeta
+from trytond.model import fields
+from trytond.pool import PoolMeta
 from trytond.transaction import Transaction
 
 __all__ = ['BankAccountParty']
