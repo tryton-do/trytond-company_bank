e15fe231a3618b236c1a7668c3c96c9616539515
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-04 15:41:57 +0100

    Migrate to version 3.4

--- tests/test_company_bank.py
+++ tests/test_company_bank.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python
-#This file is part company_bank module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part company_bank module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class CompanyBankTestCase(unittest.TestCase):
-    '''
-    Test Company Bank module.
-    '''
+    'Test Company Bank module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('company_bank')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('company_bank')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -45,6 +31,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         CompanyBankTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
